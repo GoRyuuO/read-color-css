ae8abd314bc47bb61f64d51a218c6c42f0a808b1
AFASANTH <afasan.th@gmail.com>
2021-01-10 15:12:59 +0700

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,10 +1,3 @@
-let txtArr = {
-  th: ["ม่วง", "น้ำเงิน", "เขียว", "เหลือง", "ส้ม", "แดง", "ขาว", "ดำ", "ชมพู"],
-  en: ["purple", "blue", "green", "yellow", "orenge", "red", "black", "pink"],
-};
-let output = document.querySelector(".output");
-
-
 function getRandomColor(data = txtArr, times = 1, lang = "th") {
   let output = [""];
   lang = lang.toLowerCase();
@@ -14,12 +7,16 @@ function getRandomColor(data = txtArr, times = 1, lang = "th") {
   }
   return output;
 }
-
-
+let txtArr = {
+  th: ["ม่วง", "น้ำเงิน", "เขียว", "เหลือง", "ส้ม", "แดง", "ขาว", "ดำ", "ชมพู"],
+  en: ["purple", "blue", "green", "yellow", "orenge", "red", "black", "pink"],
+  css: ["purple", "blue", "green", "yellow", "orenge", "red", "black", "#FF00FF"]
+};
+let output = document.querySelector(".output");
 function startRand() {
   document.querySelector("input#th").checked === false
     ? (lang = "en")
     : (lang = "th");
   output.innerHTML = getRandomColor(txtArr, 1, lang).join();
-  output.style.color = getRandomColor(txtArr, 1, "en").join();
-}
+  output.style.color = getRandomColor(txtArr, 1, "css").join();
+}  
