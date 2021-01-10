5e4b05945d2ab117d6325df4ada7e226c2a3c3c6
AFASANTH <afasan.th@gmail.com>
2021-01-10 13:46:01 +0700

    Split JS to another file
     - for easily to edit
     - fix a minor bug

---
+++ app/app.js
@@ -0,0 +1,25 @@
+let txtArr = {
+  th: ["ม่วง", "น้ำเงิน", "เขียว", "เหลือง", "ส้ม", "แดง", "ขาว", "ดำ", "ชมพู"],
+  en: ["purple", "blue", "green", "yellow", "orenge", "red", "black", "pink"],
+};
+let output = document.querySelector(".output");
+
+
+function getRandomColor(data = txtArr, times = 1, lang = "th") {
+  let output = [""];
+  lang = lang.toLowerCase();
+  for (let i = 0; i < times; i++) {
+    let rng = Math.floor(Math.random() * Math.floor(data[lang].length));
+    output[i] = data[lang][rng];
+  }
+  return output;
+}
+
+
+function startRand() {
+  document.querySelector("input#th").checked === false
+    ? (lang = "en")
+    : (lang = "th");
+  output.innerHTML = getRandomColor(txtArr, 1, lang).join();
+  output.style.color = getRandomColor(txtArr, 1, "en").join();
+}
